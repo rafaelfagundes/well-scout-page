5a05e3851588a303f1d3ceae4f514add59aaaf7b
Rafael Fagundes <rafaelcflima@gmail.com>
2025-02-26 18:42:39 -0500

    refactor: Update layout with custom font, theme provider, and background

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,65 @@
-// app/layout.tsx
+import type { ReactNode } from "react";
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import { Toaster } from "sonner";
+import { ThemeProvider } from "next-themes";
 
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
-import { Toaster } from 'sonner'; // Import from sonner
-
-const inter = Inter({ subsets: ['latin'] });
+const sensation = localFont({
+  src: [
+    {
+      path: "../public/fonts/Sansation_Regular.ttf",
+      weight: "400", // Regular
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Sansation_Bold.ttf",
+      weight: "700", // Bold
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Sansation_Italic.ttf",
+      weight: "400", // Regular Italic
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/Sansation_Bold_Italic.ttf",
+      weight: "700", // Bold Italic
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/Sansation_Light.ttf",
+      weight: "300", // Light
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Sansation_Light_Italic.ttf",
+      weight: "300", // Light Italic
+      style: "italic",
+    },
+  ],
+  variable: "--font-sensation",
+});
 
 export const metadata: Metadata = {
-  title: 'WellScout - Your Guide to Informed Food Choices',
-  description: 'Scan, search, and understand your products with WellScout.',
+  title: "WellScout - Your Guide to Informed Food Choices",
+  description: "Scan, search, and understand your products with WellScout.",
 };
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        {children}
-        <Toaster richColors />
+    <html lang="en" suppressHydrationWarning>
+      <body className={sensation.className}>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+          <div className="bg-mesh min-h-screen">
+            {children}
+            <Toaster richColors />
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   );
