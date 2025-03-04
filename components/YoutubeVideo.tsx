8cbd0e93261c26c6bcb2d118d526d11e57a54eba
Rafael Fagundes <rafaelcflima@gmail.com>
2025-03-04 11:43:57 -0500

    feat: Add YoutubeVideo component

---
+++ components/YoutubeVideo.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function YoutubeVideo() {
+  return (
+    <div>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/4FT19RdkdNU" title="" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen>
+      </iframe>
+    </div>
+  )
+}
+
+export default YoutubeVideo
